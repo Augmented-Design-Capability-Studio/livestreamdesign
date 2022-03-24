198fde9094a785daa0becb2cea75d322144dcb9f
Sol <94874642+solify-me@users.noreply.github.com>
2022-03-24 11:31:27 -0400

    + miro

--- livedesign-app/src/App.js
+++ livedesign-app/src/App.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState, useRef, useCallback } from "react";
-import Excalidraw, {
-  exportToCanvas,
-  exportToSvg,
-  exportToBlob
-} from "@excalidraw/excalidraw";
+// import Excalidraw, {
+//   exportToCanvas,
+//   exportToSvg,
+//   exportToBlob
+// } from "@excalidraw/excalidraw";
 import InitialData from "./initialData";
-import Sidebar from "./sidebar/Sidebar";
+//import Sidebar from "./sidebar/Sidebar";
 
 import "./App.scss";
-import initialData from "./initialData";
+//import initialData from "./initialData";
 
 import ReactTwitchEmbedVideo from "react-twitch-embed-video";
 import Draggable from "react-draggable";
@@ -25,34 +25,34 @@ const resolvablePromise = () => {
   return promise;
 };
 
-const renderTopRightUI = () => {
-  return (
-    <button onClick={() => alert("This is dummy top right UI")}>
-      {" "}
-      Click me{" "}
-    </button>
-  );
-};
+// const renderTopRightUI = () => {
+//   return (
+//     <button onClick={() => alert("This is dummy top right UI")}>
+//       {" "}
+//       Click me{" "}
+//     </button>
+//   );
+// };
 
-const renderFooter = () => {
-  return (
-    <button onClick={() => alert("This is dummy footer")}>
-      {" "}
-      custom footer{" "}
-    </button>
-  );
-};
+// const renderFooter = () => {
+//   return (
+//     <button onClick={() => alert("This is dummy footer")}>
+//       {" "}
+//       custom footer{" "}
+//     </button>
+//   );
+// };
 
 export default function App() {
   const excalidrawRef = useRef(null);
 
-  const [viewModeEnabled, setViewModeEnabled] = useState(false);
-  const [zenModeEnabled, setZenModeEnabled] = useState(false);
-  const [gridModeEnabled, setGridModeEnabled] = useState(false);
-  const [blobUrl, setBlobUrl] = useState(null);
-  const [canvasUrl, setCanvasUrl] = useState(null);
-  const [exportWithDarkMode, setExportWithDarkMode] = useState(false);
-  const [theme, setTheme] = useState("light");
+  // const [viewModeEnabled, setViewModeEnabled] = useState(false);
+  // const [zenModeEnabled, setZenModeEnabled] = useState(false);
+  // const [gridModeEnabled, setGridModeEnabled] = useState(false);
+  // const [blobUrl, setBlobUrl] = useState(null);
+  // const [canvasUrl, setCanvasUrl] = useState(null);
+  // const [exportWithDarkMode, setExportWithDarkMode] = useState(false);
+  // const [theme, setTheme] = useState("light");
 
   const initialStatePromiseRef = useRef({ promise: null });
   if (!initialStatePromiseRef.current.promise) {
@@ -94,238 +94,247 @@ export default function App() {
     };
   }, []);
 
-  const updateScene = () => {
-    const sceneData = {
-      elements: [
-        {
-          type: "rectangle",
-          version: 141,
-          versionNonce: 361174001,
-          isDeleted: false,
-          id: "oDVXy8D6rom3H1-LLH2-f",
-          fillStyle: "hachure",
-          strokeWidth: 1,
-          strokeStyle: "solid",
-          roughness: 1,
-          opacity: 100,
-          angle: 0,
-          x: 100.50390625,
-          y: 93.67578125,
-          strokeColor: "#c92a2a",
-          backgroundColor: "transparent",
-          width: 186.47265625,
-          height: 141.9765625,
-          seed: 1968410350,
-          groupIds: []
-        }
-      ],
-      appState: {
-        viewBackgroundColor: "#edf2ff"
-      }
-    };
-    excalidrawRef.current.updateScene(sceneData);
-  };
+  // const updateScene = () => {
+  //   const sceneData = {
+  //     elements: [
+  //       {
+  //         type: "rectangle",
+  //         version: 141,
+  //         versionNonce: 361174001,
+  //         isDeleted: false,
+  //         id: "oDVXy8D6rom3H1-LLH2-f",
+  //         fillStyle: "hachure",
+  //         strokeWidth: 1,
+  //         strokeStyle: "solid",
+  //         roughness: 1,
+  //         opacity: 100,
+  //         angle: 0,
+  //         x: 100.50390625,
+  //         y: 93.67578125,
+  //         strokeColor: "#c92a2a",
+  //         backgroundColor: "transparent",
+  //         width: 186.47265625,
+  //         height: 141.9765625,
+  //         seed: 1968410350,
+  //         groupIds: []
+  //       }
+  //     ],
+  //     appState: {
+  //       viewBackgroundColor: "#edf2ff"
+  //     }
+  //   };
+  //   excalidrawRef.current.updateScene(sceneData);
+  // };
 
-  const onLinkOpen = useCallback((element, event) => {
-    const link = element.link;
-    const { nativeEvent } = event.detail;
-    const isNewTab = nativeEvent.ctrlKey || nativeEvent.metaKey;
-    const isNewWindow = nativeEvent.shiftKey;
-    const isInternalLink =
-      link.startsWith("/") || link.includes(window.location.origin);
-    if (isInternalLink && !isNewTab && !isNewWindow) {
-      // signal that we're handling the redirect ourselves
-      event.preventDefault();
-      // do a custom redirect, such as passing to react-router
-      // ...
-    }
-  }, []);
+  // const onLinkOpen = useCallback((element, event) => {
+  //   const link = element.link;
+  //   const { nativeEvent } = event.detail;
+  //   const isNewTab = nativeEvent.ctrlKey || nativeEvent.metaKey;
+  //   const isNewWindow = nativeEvent.shiftKey;
+  //   const isInternalLink =
+  //     link.startsWith("/") || link.includes(window.location.origin);
+  //   if (isInternalLink && !isNewTab && !isNewWindow) {
+  //     // signal that we're handling the redirect ourselves
+  //     event.preventDefault();
+  //     // do a custom redirect, such as passing to react-router
+  //     // ...
+  //   }
+  // }, []);
 
   return (
     <div className="App">
+
       <Draggable>
         <div className="twitchwrapper">
+          drag this bar to move element
           <ReactTwitchEmbedVideo
               channel="adcs_livestreamdesign"
-              layout="video"
-              height="480" />
+              height="720" />
         </div>
       </Draggable>
+
       <Draggable>
-        <div className="chatwrapper">
-        <iframe id="twitch-chat-embed"
-          src="http://twitch.tv/adcs_livestreamdesign/chat?popout="
-          height="500"
-          width="400">
-        </iframe>
+        <div className="mirowrapper">
+        drag this bar to move element
+          <iframe
+              width="1080"
+              height="720"
+              src="https://miro.com/app/live-embed/uXjVOM3PORA=/?moveToViewport=-1540,-881,3539,1895"
+              frameBorder="0"
+              scrolling="no"
+              allowFullScreen>
+          </iframe>
         </div>
       </Draggable>
-      <Sidebar>
-        <div className="button-wrapper">
-          <button className="update-scene" onClick={updateScene}>
-            Update Scene
-          </button>
-          <button
-            className="reset-scene"
-            onClick={() => {
-              excalidrawRef.current.resetScene();
-            }}
-          >
-            Reset Scene
-          </button>
-          <button
-            onClick={() => {
-              excalidrawRef.current.updateScene({
-                libraryItems: [
-                  {
-                    status: "published",
-                    elements: initialData.libraryItems[0]
-                  },
-                  {
-                    status: "unpublished",
-                    elements: initialData.libraryItems[1]
-                  }
-                ]
-              });
-            }}
-          >
-            Update Library
-          </button>
-          <label>
-            <input
-              type="checkbox"
-              checked={viewModeEnabled}
-              onChange={() => setViewModeEnabled(!viewModeEnabled)}
-            />
-            View mode
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              checked={zenModeEnabled}
-              onChange={() => setZenModeEnabled(!zenModeEnabled)}
-            />
-            Zen mode
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              checked={gridModeEnabled}
-              onChange={() => setGridModeEnabled(!gridModeEnabled)}
-            />
-            Grid mode
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              checked={theme === "dark"}
-              onChange={() => {
-                let newTheme = "light";
-                if (theme === "light") {
-                  newTheme = "dark";
-                }
-                setTheme(newTheme);
-              }}
-            />
-            Switch to Dark Theme
-          </label>
-        </div>
-        <div className="excalidraw-wrapper">
-          <Excalidraw
-            ref={excalidrawRef}
-            initialData={initialStatePromiseRef.current.promise}
-            onChange={(elements, state) =>
-              console.info("Elements :", elements, "State : ", state)
-            }
-            onPointerUpdate={(payload) => console.info(payload)}
-            onCollabButtonClick={() =>
-              window.alert("You clicked on collab button")
-            }
-            viewModeEnabled={viewModeEnabled}
-            zenModeEnabled={zenModeEnabled}
-            gridModeEnabled={gridModeEnabled}
-            theme={theme}
-            name="Custom name of drawing"
-            UIOptions={{ canvasActions: { loadScene: false } }}
-            renderTopRightUI={renderTopRightUI}
-            renderFooter={renderFooter}
-            onLinkOpen={onLinkOpen}
-          />
-        </div>
 
-        <div className="export-wrapper button-wrapper">
-          <label className="export-wrapper__checkbox">
-            <input
-              type="checkbox"
-              checked={exportWithDarkMode}
-              onChange={() => setExportWithDarkMode(!exportWithDarkMode)}
-            />
-            Export with dark mode
-          </label>
-          <button
-            onClick={async () => {
-              const svg = await exportToSvg({
-                elements: excalidrawRef.current.getSceneElements(),
-                appState: {
-                  ...initialData.appState,
-                  exportWithDarkMode,
-                  width: 300,
-                  height: 100
-                },
-                embedScene: true,
-                files: excalidrawRef.current.getFiles()
-              });
-              document.querySelector(".export-svg").innerHTML = svg.outerHTML;
-            }}
-          >
-            Export to SVG
-          </button>
-          <div className="export export-svg"></div>
-
-          <button
-            onClick={async () => {
-              const blob = await exportToBlob({
-                elements: excalidrawRef.current.getSceneElements(),
-                mimeType: "image/png",
-                appState: {
-                  ...initialData.appState,
-                  exportWithDarkMode
-                },
-                files: excalidrawRef.current.getFiles()
-              });
-              setBlobUrl(window.URL.createObjectURL(blob));
-            }}
-          >
-            Export to Blob
-          </button>
-          <div className="export export-blob">
-            <img src={blobUrl} alt="" />
-          </div>
-
-          <button
-            onClick={async () => {
-              const canvas = await exportToCanvas({
-                elements: excalidrawRef.current.getSceneElements(),
-                appState: {
-                  ...initialData.appState,
-                  exportWithDarkMode
-                },
-                files: excalidrawRef.current.getFiles()
-              });
-              const ctx = canvas.getContext("2d");
-              ctx.font = "30px Virgil";
-              ctx.strokeText("My custom text", 50, 60);
-              setCanvasUrl(canvas.toDataURL());
-            }}
-          >
-            Export to Canvas
-          </button>
-          <div className="export export-canvas">
-            <img src={canvasUrl} alt="" />
-          </div>
-
-        </div>
-      </Sidebar>
     </div>
+      // <Sidebar>
+      //   <div className="button-wrapper">
+      //     <button className="update-scene" onClick={updateScene}>
+      //       Update Scene
+      //     </button>
+      //     <button
+      //       className="reset-scene"
+      //       onClick={() => {
+      //         excalidrawRef.current.resetScene();
+      //       }}
+      //     >
+      //       Reset Scene
+      //     </button>
+      //     <button
+      //       onClick={() => {
+      //         excalidrawRef.current.updateScene({
+      //           libraryItems: [
+      //             {
+      //               status: "published",
+      //               elements: initialData.libraryItems[0]
+      //             },
+      //             {
+      //               status: "unpublished",
+      //               elements: initialData.libraryItems[1]
+      //             }
+      //           ]
+      //         });
+      //       }}
+      //     >
+      //       Update Library
+      //     </button>
+      //     <label>
+      //       <input
+      //         type="checkbox"
+      //         checked={viewModeEnabled}
+      //         onChange={() => setViewModeEnabled(!viewModeEnabled)}
+      //       />
+      //       View mode
+      //     </label>
+      //     <label>
+      //       <input
+      //         type="checkbox"
+      //         checked={zenModeEnabled}
+      //         onChange={() => setZenModeEnabled(!zenModeEnabled)}
+      //       />
+      //       Zen mode
+      //     </label>
+      //     <label>
+      //       <input
+      //         type="checkbox"
+      //         checked={gridModeEnabled}
+      //         onChange={() => setGridModeEnabled(!gridModeEnabled)}
+      //       />
+      //       Grid mode
+      //     </label>
+      //     <label>
+      //       <input
+      //         type="checkbox"
+      //         checked={theme === "dark"}
+      //         onChange={() => {
+      //           let newTheme = "light";
+      //           if (theme === "light") {
+      //             newTheme = "dark";
+      //           }
+      //           setTheme(newTheme);
+      //         }}
+      //       />
+      //       Switch to Dark Theme
+      //     </label>
+      //   </div>
+      //   <div className="excalidraw-wrapper">
+      //     <Excalidraw
+      //       ref={excalidrawRef}
+      //       initialData={initialStatePromiseRef.current.promise}
+      //       onChange={(elements, state) =>
+      //         console.info("Elements :", elements, "State : ", state)
+      //       }
+      //       onPointerUpdate={(payload) => console.info(payload)}
+      //       onCollabButtonClick={() =>
+      //         window.alert("You clicked on collab button")
+      //       }
+      //       viewModeEnabled={viewModeEnabled}
+      //       zenModeEnabled={zenModeEnabled}
+      //       gridModeEnabled={gridModeEnabled}
+      //       theme={theme}
+      //       name="Custom name of drawing"
+      //       UIOptions={{ canvasActions: { loadScene: false } }}
+      //       renderTopRightUI={renderTopRightUI}
+      //       renderFooter={renderFooter}
+      //       onLinkOpen={onLinkOpen}
+      //     />
+      //   </div>
+      //
+      //   <div className="export-wrapper button-wrapper">
+      //     <label className="export-wrapper__checkbox">
+      //       <input
+      //         type="checkbox"
+      //         checked={exportWithDarkMode}
+      //         onChange={() => setExportWithDarkMode(!exportWithDarkMode)}
+      //       />
+      //       Export with dark mode
+      //     </label>
+      //     <button
+      //       onClick={async () => {
+      //         const svg = await exportToSvg({
+      //           elements: excalidrawRef.current.getSceneElements(),
+      //           appState: {
+      //             ...initialData.appState,
+      //             exportWithDarkMode,
+      //             width: 300,
+      //             height: 100
+      //           },
+      //           embedScene: true,
+      //           files: excalidrawRef.current.getFiles()
+      //         });
+      //         document.querySelector(".export-svg").innerHTML = svg.outerHTML;
+      //       }}
+      //     >
+      //       Export to SVG
+      //     </button>
+      //     <div className="export export-svg"></div>
+      //
+      //     <button
+      //       onClick={async () => {
+      //         const blob = await exportToBlob({
+      //           elements: excalidrawRef.current.getSceneElements(),
+      //           mimeType: "image/png",
+      //           appState: {
+      //             ...initialData.appState,
+      //             exportWithDarkMode
+      //           },
+      //           files: excalidrawRef.current.getFiles()
+      //         });
+      //         setBlobUrl(window.URL.createObjectURL(blob));
+      //       }}
+      //     >
+      //       Export to Blob
+      //     </button>
+      //     <div className="export export-blob">
+      //       <img src={blobUrl} alt="" />
+      //     </div>
+      //
+      //     <button
+      //       onClick={async () => {
+      //         const canvas = await exportToCanvas({
+      //           elements: excalidrawRef.current.getSceneElements(),
+      //           appState: {
+      //             ...initialData.appState,
+      //             exportWithDarkMode
+      //           },
+      //           files: excalidrawRef.current.getFiles()
+      //         });
+      //         const ctx = canvas.getContext("2d");
+      //         ctx.font = "30px Virgil";
+      //         ctx.strokeText("My custom text", 50, 60);
+      //         setCanvasUrl(canvas.toDataURL());
+      //       }}
+      //     >
+      //       Export to Canvas
+      //     </button>
+      //     <div className="export export-canvas">
+      //       <img src={canvasUrl} alt="" />
+      //     </div>
+      //
+      //   </div>
+      // </Sidebar>
+
+    //</div>
   );
 }
